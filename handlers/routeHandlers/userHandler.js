bafc921941a98517230daa7c5751e07d7b3e96c7
taauntik <devauntik@gmail.com>
2021-12-12 22:21:27 +0600

    implemented the user put functionality

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -118,7 +118,76 @@ handler._users.get = (requestProperties, callback) => {
     }
 };
 
-handler._users.put = (requestProperties, callback) => {};
+handler._users.put = (requestProperties, callback) => {
+    const firstName =
+        typeof requestProperties.body.firstName === 'string' &&
+        requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : false;
+
+    const lastName =
+        typeof requestProperties.body.lastName === 'string' &&
+        requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : false;
+
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+        requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+
+    const password =
+        typeof requestProperties.body.password === 'string' &&
+        requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : false;
+
+    if (phone) {
+        if (firstName || lastName || password) {
+            // look up the user
+            data.read('users', phone, (err1, user) => {
+                const userData = { ...parseJSON(user) };
+                if (!err1 && userData) {
+                    if (firstName) {
+                        userData.firstName = firstName;
+                    }
+                    if (lastName) {
+                        userData.lastName = lastName;
+                    }
+                    if (password) {
+                        userData.password = hash(password);
+                    }
+
+                    // store to database
+                    data.update('users', phone, userData, (err2) => {
+                        if (!err2) {
+                            callback(200, {
+                                message: 'User was updated successfully',
+                            });
+                        } else {
+                            callback(500, {
+                                error: 'There was a problem in the server side',
+                            });
+                        }
+                    });
+                } else {
+                    callback(400, {
+                        error: 'You have a problem in your request!',
+                    });
+                }
+            });
+        } else {
+            callback(400, {
+                error: 'You have a problem in your request',
+            });
+        }
+    } else {
+        callback(400, {
+            error: 'Invalid phone number, please try again',
+        });
+    }
+};
 
 handler._users.delete = (requestProperties, callback) => {};
 
