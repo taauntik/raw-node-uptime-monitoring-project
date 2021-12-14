00b5c02f42019770df3901c3d3f18febf80c6b5f
taauntik <devauntik@gmail.com>
2021-12-14 20:02:53 +0600

    added some files and made the boiler plate code of worker.js

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 /**
- * Title: Uptime Monitoring Application
- * Description: a RESTFul API to monitor up or down time of user defined links
+ * Title: Project Initial File
+ * Description: Initial file to start to node server and workers
  * Author: Tasnim Ahmed Auntik
- * Date: 11/12/2021
+ * Date: 14/12/2021
  */
 
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
-const { sendTwilioSms } = require('./helpers/notifications');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 // app object - module scaffolding
 const app = {};
 
-// @TODO remove later
-sendTwilioSms('01630958759', 'Hello Auntik, This is Auntik from another side', (err) => {
-    console.log(`This is the error ${err}`);
-});
-
-// create server
-app.createServer = () => {
-    const { handleReqRes: handleRequestResponse } = app;
-    const { port } = environment;
-    const server = http.createServer(handleRequestResponse);
-    server.listen(port, () => {
-        console.log(`listening port number ${port}`);
-    });
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
 };
 
-// handle request response
-app.handleReqRes = handleReqRes;
+app.init();
 
-// start the server
-app.createServer();
+// export the app
+module.exports = app;
